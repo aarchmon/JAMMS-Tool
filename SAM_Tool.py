9a207102debb1f07d5290595d39e9a8a69889b5c
Aaron Montano <aarchmon@Aarons-MacBook-Pro.local>
2021-02-19 13:58:44 -0800

    Removed src folder.

--- SAM_Tool.py
+++ SAM_Tool.py
@@ -3,9 +3,9 @@ from pathlib import Path
 import fire
 
 # Import profiler functions.
-from ..include.profiler import get_info
-from ..include.profiler import qualification
-from ..include.profiler import risk_profile
+from include.profiler import get_info
+from include.profiler import qualification
+from include.profiler import risk_profile
                   
 def run():
     """The main function for running the script."""
