616c062192d186276b370d571a8a0fbffdcab171
sarahkang714 <sarahkang714@gmail.com>
2021-02-24 18:48:07 -0800

    adding efficient frontier to RUN

--- src/include/visualizer.py
+++ src/include/visualizer.py
@@ -4,7 +4,7 @@ Visualizer Module
 Visualizer module is responsible for outputting user-friendly visual aides. 
 """
 
-from include.MCForcastTools import MCSimulation
+from MCForcastTools import MCSimulation
 
 def create_MC_object(df, weight, num_sims, num_trade_days):
     """
